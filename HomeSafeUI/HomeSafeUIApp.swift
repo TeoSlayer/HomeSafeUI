a9a7f12339a8e039dd72c11b07a25ec106c428e6
Teo Slayer <teodor.calin@me.com>
2022-02-22 01:21:06 +0000

    Initial Commit

---
+++ HomeSafeUI/HomeSafeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSafeUIApp.swift
+//  HomeSafeUI
+//
+//  Created by Calin Teodor on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeSafeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
